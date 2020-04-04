c1c0ae51c317c53192d2502f387dd97c2caf291e
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-04-04 12:49:15 +0200

    #10 -> Enemy behaviour ( attack, idle, look at.. )

---
+++ 3Ways/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Animator anime;
+    protected bool bAttack;
+
+    private Transform player;
+
+    void Start()
+    {
+        bAttack = false;
+        anime = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    public void OnTriggerEnter()
+    {
+        if(anime != null)
+        {
+            bAttack = true;
+            StartCoroutine(Attack(true));
+        }
+    }
+
+    public void OnTriggerExit()
+    {
+        if (anime != null)
+        {
+            bAttack = false;
+            StartCoroutine(Attack(false));
+        }
+    }
+    private IEnumerator Attack(bool atck)
+    {
+        yield return new WaitForSeconds(.75f);
+        anime.SetBool("attack", atck);
+    }
+    
+    void Update()
+    {
+        transform.LookAt(player.position);
+    }
+}
