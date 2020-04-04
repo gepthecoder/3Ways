7b5dc62ebcc8313b26e3207ff947e152c6357aed
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-04-04 12:46:30 +0200

    #8 ->Choose door script

---
+++ 3Ways/Assets/Scripts/Player/ChooseDoor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseDoor : MonoBehaviour
+{
+    public static int selectedDoor;
+    public static bool doorPressed;
+    public static bool doorChoosen;
+
+    public enum Doors
+    {
+        DOOR0 = 0, DOOR1, DOOR2,
+    }
+
+    void Update()
+    {
+        if (StateMachine.iCurrentState == (int)StateMachine.PlayerStates.THINKING && PlayerControl.canChooseDoor)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if(hit.transform.tag == "SF_Door")
+                    {
+                        Debug.Log("touched gameobject name: " + hit.transform.name);
+
+                        doorChoosen = true;
+                        doorPressed = true;
+                        if (hit.transform.name == "Door0")
+                        {
+                            // player chose DOOR 0
+                            Debug.Log("door 0 choosen!");
+                            selectedDoor = (int)Doors.DOOR0;
+                        }
+                        else if (hit.transform.name == "Door1")
+                        {
+                            // player chose DOOR 1
+                            Debug.Log("door 1 choosen!");
+                            selectedDoor = (int)Doors.DOOR1;
+                        }
+                        else if (hit.transform.name == "Door2")
+                        {
+                            // player chose DOOR 2
+                            Debug.Log("door 2 choosen!");
+                            selectedDoor = (int)Doors.DOOR2;
+                        }
+
+                        PlayerControl.canChooseDoor = false;
+
+                    }
+
+                }
+            }
+        }
+    }
+}
