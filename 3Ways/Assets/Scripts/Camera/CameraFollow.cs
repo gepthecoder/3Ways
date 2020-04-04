b8ac8f96882a78e4b36106f5f0c6fd35694a4c3f
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-04-04 12:52:45 +0200

    #13 -> CameraFollow script

---
+++ 3Ways/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform lookTarget;
+
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothPos;
+
+        transform.LookAt(lookTarget);
+    }
+}
