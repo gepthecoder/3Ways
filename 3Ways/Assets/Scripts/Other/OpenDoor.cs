422f6c894b18cd2952aed29284eea3d6d7b62495
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-04-04 12:53:43 +0200

    #14 -> OpenDoor & StopCollider script

---
+++ 3Ways/Assets/Scripts/Other/OpenDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public static bool inFrontOfDoor = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        inFrontOfDoor = true;
+        Debug.Log("<color=blue>IN FRONT OF DOOR!!</color>");
+
+    }
+}
---
+++ 3Ways/Assets/Scripts/Other/StopCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopCollider : MonoBehaviour
+{
+    public static bool choosingPosition;
+
+    void Start()
+    {
+        choosingPosition = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //TO:DO
+            //      -> stop the player if the door wasn't pre-selected
+            //      -> think position
+            //      -> waiting player to choose the right door 
+
+            Debug.Log("Choosing door...");
+            choosingPosition = true;
+        }
+    }
+
+}
