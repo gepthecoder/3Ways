3ffd95f98de090af2fa37cd912e15c85800a2ae0
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-04-17 22:00:44 +0200

    #38 -> Game Settings, Pause Menu with animations

--- 3Ways/Assets/Scripts/Other/UIManager.cs
+++ 3Ways/Assets/Scripts/Other/UIManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator CanvasAnimator;
+    private Animator settingsUIanime;
+
+
     void Start()
     {
-        
+        CanvasAnimator = GetComponent<Animator>();
+        settingsUIanime = settingsUI.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                  P A U S E   M E N U                                          //
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    
+    public void OpenPauseMenu()
     {
-        
+        Time.timeScale = 0;
+        CanvasAnimator.SetTrigger("showPauseMenu");   
+    }
+
+    public void ClosePauseMenu()
+    {
+        Time.timeScale = 1;
+        CanvasAnimator.SetTrigger("closePauseMenu");
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                  I N - G A M E  S E T T I N G S                               //
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public GameObject settingsUI;
+
+    public void ShowSettings()
+    {
+        settingsUIanime.SetTrigger("showSettings");
+    }
+
+    public void HideSettings()
+    {
+        settingsUIanime.SetTrigger("hideSettings");
     }
 }
