f6670654b730f3f2f88d00e406054e6e17ece001
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-05-10 22:58:38 +0200

    #53 -> Dance Move Shop (6 dances to choose from) + Scripting

--- 3Ways/Assets/Scripts/CampaignScene/InGameShop.cs
+++ 3Ways/Assets/Scripts/CampaignScene/InGameShop.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class InGameShop : MonoBehaviour
 {
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    // CHARACTERS + CROWNS
     private int iCurrentCharacter;
 
     protected const int CHARACTER_PRICE_OG = 100000;
@@ -31,21 +34,137 @@ public class InGameShop : MonoBehaviour
 
     public Animator ANIME_SHOP;
 
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    // DANCE MOVES
+    private int iCurrentDanceMove;
+
+    protected const int DANCE_MOVE_1 = 25000;
+    protected const int DANCE_MOVE_2 = 50000;
+    protected const int DANCE_MOVE_3 = 75000;
+    protected const int DANCE_MOVE_4 = 100000;
+    protected const int DANCE_MOVE_5 = 1000000;
+
+    private int D1_BOUGHT = 0;
+    private int D2_BOUGHT = 0;
+    private int D3_BOUGHT = 0;
+
+    private int D4_BOUGHT = 0;
+    private int D5_BOUGHT = 0;
+
+
+    public GameObject PRICE_GUI_D1;
+    public GameObject USE_BUTTON_D1;
+    public GameObject BUY_BUTTON_D1;
+    
+    public GameObject PRICE_GUI_D2;
+    public GameObject USE_BUTTON_D2;
+    public GameObject BUY_BUTTON_D2;
+
+    public GameObject PRICE_GUI_D3;
+    public GameObject USE_BUTTON_D3;
+    public GameObject BUY_BUTTON_D3;
+
+    public GameObject PRICE_GUI_D4;
+    public GameObject USE_BUTTON_D4;
+    public GameObject BUY_BUTTON_D4;
+
+    public GameObject PRICE_GUI_D5;
+    public GameObject USE_BUTTON_D5;
+    public GameObject BUY_BUTTON_D5;
+
+
+    public GameObject FRAME_TRIPPING_D0;
+    public GameObject FRAME_TRIPPING_D1;
+    public GameObject FRAME_TRIPPING_D2;
+    public GameObject FRAME_TRIPPING_D3;
+    public GameObject FRAME_TRIPPING_D4;
+    public GameObject FRAME_TRIPPING_D5;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    // GUI HANDLER -> ALL CATEGORIES
+    public GameObject NodeChar;
+
+    public GameObject NodeDance;
+    public GameObject NodeDancePageOne;
+    public GameObject NodeDancePageTwo;
+
+    public GameObject NodePowerUps;
+
+    public void SetShopNode(int node)
+    {
+        switch (node)
+        {
+            case 0:
+                NodeChar.SetActive(true);
+                NodeDance.SetActive(false);
+                NodePowerUps.SetActive(false);
+                break;
+
+            case 1:
+                NodeChar.SetActive(false);
+                NodeDance.SetActive(true);
+                NodePowerUps.SetActive(false);
+                break;
+
+            case 2:
+                NodeChar.SetActive(false);
+                NodeDance.SetActive(false);
+                NodePowerUps.SetActive(true);
+                break;
+        }
+    }
+
+    public void SecondDancePage()
+    {
+        NodeDancePageTwo.SetActive(true);
+        NodeDancePageOne.SetActive(false);
+    }
+    public void FirstDancePage()
+    {
+        NodeDancePageTwo.SetActive(false);
+        NodeDancePageOne.SetActive(true);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////
+
+
+
     void Awake()
     {
+        // CHARACTERS + CROWNS
         GetPrefsChar();
         GetPrefsCharBought();
         GetPrefsCrowns();
+        //
+
+        // DANCE MOVES
+        GetPrefsDance();
+        GetPrefsDancesBought();
+        //
     }
 
     void Start()
     {
+        // CHARACTERS + CROWNS
         SetTextCrowns();
         SetFrame(iCurrentCharacter);
         HandleGUIs(OG_BOUGHT, EX_BOUGHT);
+        //
+
+        // DANCE MOVES
+        SetDanceFrame(iCurrentDanceMove);
+        HandleDanceGUIs(D1_BOUGHT, D2_BOUGHT, D3_BOUGHT, D4_BOUGHT, D5_BOUGHT);
+        //
     }
 
-    // PREFS
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    // PREFS -> CHARACTER SHOP
 
     private void GetPrefsChar()
     {
@@ -232,6 +351,295 @@ public class InGameShop : MonoBehaviour
         }
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////
 
 
+    // PREFS -> DANCE MOVES SHOP
+
+    private void GetPrefsDance()
+    {
+        if (PlayerPrefs.HasKey("iCurrentDanceMove"))
+        {
+            //we had a previous session
+            iCurrentDanceMove = PlayerPrefs.GetInt("iCurrentDanceMove", 0);
+        }
+        else
+        {
+            SetPrefsDance();
+        }
+    }
+
+    private void SetPrefsDance()
+    {
+        PlayerPrefs.SetInt("iCurrentDanceMove", iCurrentDanceMove);
+    }
+
+
+    private void GetPrefsDancesBought()
+    {
+        if (PlayerPrefs.HasKey("D1_BOUGHT") ||  PlayerPrefs.HasKey("D2_BOUGHT") ||
+            PlayerPrefs.HasKey("D3_BOUGHT") ||  PlayerPrefs.HasKey("D4_BOUGHT") ||
+                                                PlayerPrefs.HasKey("D5_BOUGHT"))
+        {
+            //we had a previous session
+            D1_BOUGHT = PlayerPrefs.GetInt("D1_BOUGHT", 0);
+            D2_BOUGHT = PlayerPrefs.GetInt("D2_BOUGHT", 0);
+            D3_BOUGHT = PlayerPrefs.GetInt("D3_BOUGHT", 0);
+            D4_BOUGHT = PlayerPrefs.GetInt("D4_BOUGHT", 0);
+            D5_BOUGHT = PlayerPrefs.GetInt("D5_BOUGHT", 0);
+        }
+        else
+        {
+            SetPrefsDanceBought();
+        }
+    }
+
+    private void SetPrefsDanceBought()
+    {
+        PlayerPrefs.SetInt("D1_BOUGHT", D1_BOUGHT);
+        PlayerPrefs.SetInt("D2_BOUGHT", D2_BOUGHT);
+        PlayerPrefs.SetInt("D3_BOUGHT", D3_BOUGHT);
+        PlayerPrefs.SetInt("D4_BOUGHT", D4_BOUGHT);
+        PlayerPrefs.SetInt("D5_BOUGHT", D5_BOUGHT);
+    }
+
+    //
+
+    // BUTTON EVENTS
+
+    public void BuyDanceMove1()
+    {
+        if (iCurrentCrowns >= DANCE_MOVE_1)
+        {
+            //buy item
+            iCurrentCrowns -= DANCE_MOVE_1;
+            SetPrefsCrowns();
+            SetTextCrowns();
+
+            D1_BOUGHT = 1;
+            SetPrefsDanceBought();
+
+            Destroy(PRICE_GUI_D1);
+            Destroy(BUY_BUTTON_D1);
+            USE_BUTTON_D1.SetActive(true);
+        }
+        else { Debug.Log("Declined buy! -> no resources"); }
+    }
+
+    public void BuyDanceMove2()
+    {
+        if (iCurrentCrowns >= DANCE_MOVE_2)
+        {
+            //buy item
+            iCurrentCrowns -= DANCE_MOVE_2;
+            SetPrefsCrowns();
+            SetTextCrowns();
+
+            D2_BOUGHT = 1;
+            SetPrefsDanceBought();
+
+            Destroy(PRICE_GUI_D2);
+            Destroy(BUY_BUTTON_D2);
+            USE_BUTTON_D2.SetActive(true);
+        }
+        else { Debug.Log("Declined buy! -> no resources"); }
+    }
+
+    public void BuyDanceMove3()
+    {
+        if (iCurrentCrowns >= DANCE_MOVE_3)
+        {
+            //buy item
+            iCurrentCrowns -= DANCE_MOVE_3;
+            SetPrefsCrowns();
+            SetTextCrowns();
+
+            D3_BOUGHT = 1;
+            SetPrefsDanceBought();
+
+            Destroy(PRICE_GUI_D3);
+            Destroy(BUY_BUTTON_D3);
+            USE_BUTTON_D3.SetActive(true);
+        }
+        else { Debug.Log("Declined buy! -> no resources"); }
+    }
+
+    public void BuyDanceMove4()
+    {
+        if (iCurrentCrowns >= DANCE_MOVE_4)
+        {
+            //buy item
+            iCurrentCrowns -= DANCE_MOVE_4;
+            SetPrefsCrowns();
+            SetTextCrowns();
+
+            D4_BOUGHT = 1;
+            SetPrefsDanceBought();
+
+            Destroy(PRICE_GUI_D4);
+            Destroy(BUY_BUTTON_D4);
+            USE_BUTTON_D4.SetActive(true);
+        }
+        else { Debug.Log("Declined buy! -> no resources"); }
+    }
+
+    public void BuyDanceMove5()
+    {
+        if (iCurrentCrowns >= DANCE_MOVE_5)
+        {
+            //buy item
+            iCurrentCrowns -= DANCE_MOVE_5;
+            SetPrefsCrowns();
+            SetTextCrowns();
+
+            D5_BOUGHT = 1;
+            SetPrefsDanceBought();
+
+            Destroy(PRICE_GUI_D5);
+            Destroy(BUY_BUTTON_D5);
+            USE_BUTTON_D5.SetActive(true);
+        }
+        else { Debug.Log("Declined buy! -> no resources"); }
+    }
+    
+    public void UseDanceMove(int iChoosen)
+    {
+        iCurrentDanceMove = iChoosen;
+        SetPrefsDance();
+        SetDanceFrame(iChoosen);
+    }
+
+    //
+
+    // HANDLERS
+
+    private void SetDanceFrame(int iChoosen)
+    {
+        switch (iChoosen)
+        {
+            case 0:
+                FRAME_TRIPPING_D0.SetActive(true);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(false);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            case 1:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(true);
+                FRAME_TRIPPING_D2.SetActive(false);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            case 2:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(true);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            case 3:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(true);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            case 4:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(false);
+                FRAME_TRIPPING_D3.SetActive(true);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            case 5:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(false);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(true);
+                FRAME_TRIPPING_D5.SetActive(false);
+                break;
+            default:
+                FRAME_TRIPPING_D0.SetActive(false);
+                FRAME_TRIPPING_D1.SetActive(false);
+                FRAME_TRIPPING_D2.SetActive(false);
+                FRAME_TRIPPING_D3.SetActive(false);
+                FRAME_TRIPPING_D4.SetActive(false);
+                FRAME_TRIPPING_D5.SetActive(true);
+                break;
+        }
+    }
+
+    private void HandleDanceGUIs(int D1, int D2, int D3, int D4, int D5)
+    {
+        if (BUY_BUTTON_D1 != null)
+        {
+            if (D1 == 1)
+            {
+                //remove price tag
+                //substitute buy with use btn
+
+                Destroy(PRICE_GUI_D1);
+                Destroy(BUY_BUTTON_D1);
+                USE_BUTTON_D1.SetActive(true);
+            }
+        }
+
+        if (BUY_BUTTON_D2 != null)
+        {
+            if (D2 == 1)
+            {
+                //remove price tag
+                //substitute buy with use btn
+
+                Destroy(PRICE_GUI_D2);
+                Destroy(BUY_BUTTON_D2);
+                USE_BUTTON_D2.SetActive(true);
+            }
+        }
+        if (BUY_BUTTON_D3 != null)
+        {
+            if (D3 == 1)
+            {
+                //remove price tag
+                //substitute buy with use btn
+
+                Destroy(PRICE_GUI_D3);
+                Destroy(BUY_BUTTON_D3);
+                USE_BUTTON_D3.SetActive(true);
+            }
+        }
+        if (BUY_BUTTON_D4 != null)
+        {
+            if (D4 == 1)
+            {
+                //remove price tag
+                //substitute buy with use btn
+
+                Destroy(PRICE_GUI_D4);
+                Destroy(BUY_BUTTON_D4);
+                USE_BUTTON_D4.SetActive(true);
+            }
+        }
+        if (BUY_BUTTON_D5 != null)
+        {
+            if (D5 == 1)
+            {
+                //remove price tag
+                //substitute buy with use btn
+
+                Destroy(PRICE_GUI_D5);
+                Destroy(BUY_BUTTON_D5);
+                USE_BUTTON_D5.SetActive(true);
+            }
+        }
+    }
 }
